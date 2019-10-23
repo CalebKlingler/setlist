b5fc8b3abaf3426422c78be1cbc671e0787ce28e
Caleb Klingler <cjkling6@gmail.com>
2019-10-22 19:26:04 -0500

    initial commit

---
+++ src/test/java/edu/launchcode/setlist/SetlistApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.launchcode.setlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
